52ed895392ba8f4dbb43ae8b264ca406ffd32176
youssef hany <youssef.hossam2435@gmail.com>
2021-08-25 11:08:38 +0200

    login and register page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Welcome from './components/Welcome';
 import MoviesPage from './components/MoviesPage'
+import LoginApp from './components/login/loginApp'
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,8 @@ function App() {
           <Route exact path="/" component={Welcome}/>
 
           <Route path="/movies" component={MoviesPage}/>
+
+          <Route path="/login" component={LoginApp}/>
         </Switch>
       </BrowserRouter>
      
